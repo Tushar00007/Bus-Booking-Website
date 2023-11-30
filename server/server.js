b6e9595df59f3ae2e7c9bb1558be32de82bd04bb
Ankit Singh <52825138+unkitsingh0@users.noreply.github.com>
2023-11-30 09:46:45 +0530

    updating keys

--- server/server.js
+++ server/server.js
@@ -6,10 +6,8 @@ import districtNameRouter from "./routes/districtNameRouter.js";
 import { connectToDb } from "./model/database.js";
 import cors from "cors";
 dotenv.config();
-// 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
-let stripeInstance = stripe(
-  "sk_test_51NmyexSBxwSrzID7jeELp3z6LXMDkxH0LfrNvooanykSD1fm4oCyhfN17LvKIACivJflTFwkF0xfwdQ4f2VnIrQm00jDSLVufl"
-);
+
+let stripeInstance = stripe(process.env.STRIPTOKEN);
 const app = express();
 const PORT = process.env.PORT || 8035;
 
