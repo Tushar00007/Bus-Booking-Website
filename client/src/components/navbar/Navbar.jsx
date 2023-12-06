4b9a963e7351043f4e3c48646ae21eb27089f018
shubham4595 <127825956+shubham4595@users.noreply.github.com>
2023-12-06 10:48:40 +0530

    Changes

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,85 +1,4 @@
-// import React from "react";
-// import { NavLink, Link } from "react-router-dom";
-// import DirectionsBusFilledIcon from "@mui/icons-material/DirectionsBusFilled";
-// function Navbar() {
-//   return (
-//     <div>
-//       <nav className="navbar navbar-expand-lg bg-body-tertiary">
-//         <div className="container-fluid">
-//           <Link className="navbar-brand" to={"/"} style={{ color: "#3f51b5" }}>
-//             <DirectionsBusFilledIcon style={{ fontSize: 25 }} />
-//             ViaBook
-//           </Link>
-//           <button
-//             className="navbar-toggler"
-//             type="button"
-//             data-bs-toggle="collapse"
-//             data-bs-target="#navbarSupportedContent"
-//             aria-controls="navbarSupportedContent"
-//             aria-expanded="false"
-//             aria-label="Toggle navigation"
-//           >
-//             <span className="navbar-toggler-icon" />
-//           </button>
-//           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-//             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-//               <li className="nav-item">
-//                 <NavLink className="nav-link" to="/">
-//                   Home
-//                 </NavLink>
-//               </li>
-//               <li className="nav-item">
-//                 <a className="nav-link" href="#">
-//                   Link
-//                 </a>
-//               </li>
-//               <li className="nav-item dropdown">
-//                 <a
-//                   className="nav-link dropdown-toggle"
-//                   href="#"
-//                   role="button"
-//                   data-bs-toggle="dropdown"
-//                   aria-expanded="false"
-//                 >
-//                   Dropdown
-//                 </a>
-//                 <ul className="dropdown-menu">
-//                   <li>
-//                     <a className="dropdown-item" href="#">
-//                       Action
-//                     </a>
-//                   </li>
-//                   <li>
-//                     <a className="dropdown-item" href="#">
-//                       Another action
-//                     </a>
-//                   </li>
-//                   <li>
-//                     <hr className="dropdown-divider" />
-//                   </li>
-//                   <li>
-//                     <a className="dropdown-item" href="#">
-//                       Something else here
-//                     </a>
-//                   </li>
-//                 </ul>
-//               </li>
-//               <li className="nav-item">
-//                 <a className="nav-link disabled" aria-disabled="true">
-//                   Disabled
-//                 </a>
-//               </li>
-//             </ul>
-//           </div>
-//         </div>
-//       </nav>
-//     </div>
-//   );
-// }
 
-// export default Navbar;
-
-/// mui
 
 import * as React from "react";
 import AppBar from "@mui/material/AppBar";
@@ -90,22 +9,22 @@ import Typography from "@mui/material/Typography";
 import Menu from "@mui/material/Menu";
 import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
+//import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
+//import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
 
 import { NavLink, useNavigate } from "react-router-dom";
 import DirectionsBusFilledIcon from "@mui/icons-material/DirectionsBusFilled";
-import styled from "@emotion/styled";
+//import styled from "@emotion/styled";
 
 const pages = ["Home"];
-const settings = ["Profile", "Account", "Dashboard", "Logout"];
+//const settings = ["Profile", "Account", "Dashboard", "Logout"];
 
 function ResponsiveAppBar() {
   let navigate = useNavigate();
   const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const [ setAnchorElUser] = React.useState(null);
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
@@ -149,7 +68,7 @@ function ResponsiveAppBar() {
               navigate("/");
             }}
           >
-            ViaBook
+            MakeMyTrip
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
@@ -205,7 +124,9 @@ function ResponsiveAppBar() {
             variant="h5"
             noWrap
             component="a"
+
             // href="#app-bar-with-responsive-menu"
+
             sx={{
               mr: 2,
               display: { xs: "flex", md: "none" },
@@ -220,7 +141,7 @@ function ResponsiveAppBar() {
               navigate("/");
             }}
           >
-            ViaBook
+            MakeMyTrip
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
