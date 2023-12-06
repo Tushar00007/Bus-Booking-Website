d3c194b190918c0a5a8485dc51f4e3abb7adf4dd
shubham4595 <127825956+shubham4595@users.noreply.github.com>
2023-12-06 10:46:22 +0530

    Update Footer.js

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -1,29 +1,19 @@
 import React from "react";
-import InstagramIcon from "@mui/icons-material/Instagram";
-import TwitterIcon from "@mui/icons-material/Twitter";
+
 import DirectionsBusFilledIcon from "@mui/icons-material/DirectionsBusFilled";
 function Footer() {
   return (
-    <footer className="footer">
+    <footer className="footer" style={{backgroundColor:"#323232"}}>
       <div className="firstHalf">
         <h3>
           <DirectionsBusFilledIcon style={{ fontSize: 30 }} />
-          ViaBook
+          MakeMyTrip
         </h3>
-        <p>When you have a chois .Chose ViaBook </p>
+        <p> Nothing is Better than MakeMyTrip </p>
 
-        <p>
-          ViaBook offer bus ticket booking through its website ,IOS,and android
-          mobile apps for all major cities.
-        </p>
-
-        <p>ViaBook@ViaBook.com </p>
+        <p>MakeMyTrip@MakeMyTrip.com </p>
       </div>
-      {/* <div className="secondHalf">
-        <h3>Follow Us</h3>
-        <InstagramIcon />
-        <TwitterIcon />
-      </div> */}
+     
     </footer>
   );
 }
