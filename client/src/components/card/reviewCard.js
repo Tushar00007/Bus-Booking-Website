9a015f6d19ddcbd4e3428d962a65b7aae4e05a43
shubham4595 <127825956+shubham4595@users.noreply.github.com>
2023-12-06 10:44:40 +0530

    Update reviewCard.js

--- client/src/components/card/reviewCard.js
+++ client/src/components/card/reviewCard.js
@@ -3,21 +3,22 @@ import { Card, CardContent, Avatar, Typography, Rating } from "@mui/material";
 
 const CustomerReviewCard = ({ customerName, avatarSrc, rating, message }) => {
   return (
-    <Card sx={{ maxWidth: 345 }} style={{ margin: "10px" }}>
+    <Card sx={{ maxWidth: 345 }} style={{ margin: "10px"}}>
       <CardContent sx={{ width: 350 }}>
         <Avatar
           alt={customerName}
           src={avatarSrc}
           sx={{ margin: "auto", padding: "2rem" }}
+          style={{fontFamily:"cursive"}}
         />
-        <Typography variant="h6" component="div" style={{ marginTop: 10 }}>
+        <Typography variant="h6" component="div" style={{ marginTop: 10 ,fontFamily:"cursive"}}>
           {customerName}
         </Typography>
         <Rating value={rating} readOnly precision={0.5} />
         <Typography
           variant="body2"
           color="text.secondary"
-          style={{ marginTop: 10 }}
+          style={{ marginTop: 10, fontFamily:"italic" }}
         >
           {message}
         </Typography>
